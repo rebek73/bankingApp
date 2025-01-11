59f56659b926d32a980342445c23f1de1e1a4cd2
rebek73 <rebemon95@outlook.com>
2025-01-10 20:32:57 -0600

    add balance section on the dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = {
+    firstName: "Rebecca"
+  }
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome to Horizon"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext= "Access and manage your account & transactions efficiently from anywhere"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+
+        </header>
+      </div>
+    </section>
   )
 }
 
